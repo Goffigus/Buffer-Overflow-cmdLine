d01d95a884170550b3c54ab936b0b5dfe0f968c6
Benjamin Goff <goffigus@gmail.com>
2020-06-26 13:09:58 -0400

    Add project files.

---
+++ Buffer Overflow cmdLine/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using static System.Console;
+
+namespace Buffer_Overflow_cmdLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cost = 11999;
+            int quantity = 0;
+            int total = 0;
+            while (true)
+            {
+                Console.WriteLine("Cost of Item is $11,999");
+                Console.WriteLine("Quantity being purchased:");
+                quantity = Convert.ToInt32(ReadLine()); //357943
+                total = cost * quantity;
+                Console.WriteLine("Total is $" + total + '\n');
+
+            }
+            
+        }
+    }
+}
